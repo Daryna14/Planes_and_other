e792b1d93061802d01b9a11f20bd95e1f110a7cf
Daryna14 <gusak.darina20@gmail.com>
2022-12-10 03:43:48 +0300

    my tests commit

--- src/main/java/org/prog/Main.java
+++ src/main/java/org/prog/Main.java
@@ -1,4 +1,24 @@
 package org.prog;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Plane Hurricane = new Plane();
+        String[] passengerName = new String[6];
+
+        passengerName[0] = "John Smith";
+        passengerName[1] = "Bella Donna";
+        passengerName[2] = "Oliver Twist";
+        passengerName[3] = "Sherlock Holmes";
+        passengerName[4] = "Harry Potter";
+        passengerName[5] = "Robinson Crusoe";
+
+        Plane.setPassengerName(passengerName);
+
+        for (int i = 0; i < Hurricane.passengers.length; i++) {
+            System.out.println(Hurricane.getPassengerName(i));
+
+        }
+    }
 }
---
+++ src/main/java/org/prog/Plane.java
@@ -0,0 +1,28 @@
+package org.prog;
+
+public class Plane {
+    public static void Plane(String... args) {
+        Main Hurricane = new Main();
+
+    }
+    public static void setPassengerName(String[] passengerName) {
+    }
+
+    public String[] passengers = new String[6];
+
+
+    public String[] getPassengerName(int i) {
+        String[] passengerName = new String[0];
+        return passengerName;
+    }
+
+}
+
+
+
+
+
+
+
+
+
